ee633001ba226274dba4a35b0254fb1830768dcd
gogulprasad <gogulprasad@gmail.com>
2020-07-17 15:40:43 +0800

    Added start/stop button, cleaned code

--- appscripts/main.js
+++ appscripts/main.js
@@ -5,6 +5,39 @@ import Exchange from "/components/Exchange.js";
 import RachfordRice from "/components/RachfordRice.js";
 import Plot from "/components/Plot.js"
 
+// Free inputs for this javascript file
+// Preselected components A and B
+let preselectA = "n-Pentane";
+let preselectB = "n-Heptane";
+
+// Slider range and starting T,P and zA
+let Tmin = 0; let T = 50; let Tmax = 200;
+let Pmin = 1; let P = 101;  let Pmax = 1000;
+let zA = 0.5;
+
+// Visible volume fraction of vapor to total container
+let volVap = 0.8;
+
+// Padding box drawing
+let padding = 10;
+
+// 
+const lidH = 0;
+const particleRadius = 10;
+let numParticles = 20;
+
+// Plot Limits
+let Tmin_plot = Tmin;
+let Tmax_plot = Tmax;
+let step = 1;
+
+
+// Add others here so that we don't have to modify in between code
+
+
+
+
+
 // Grab the div where we will put our Raphael paper
 let centerDiv = document.getElementById("centerDiv");
 
@@ -12,17 +45,19 @@ let centerDiv = document.getElementById("centerDiv");
 let paper = new Raphael(centerDiv);
 
 // RachFordRice Calculations
+let compAselect = document.getElementById("compAselect");
+let compBselect = document.getElementById("compBselect");
 let Tslider = document.getElementById("Tslider");
 let Pslider = document.getElementById("Pslider");
 let Zslider = document.getElementById("Zslider");
-let compAselect = document.getElementById("compAselect");
-let compBselect = document.getElementById("compBselect");
 
+// Listing the choices of components
 let optArrayA = [];
 let optArrayB = [];
 for (let i = 0; i < RachfordRice.chemicals.length; i++){
     let optA = document.createElement("option");
-    let optB =  document.createElement("option")
+    let optB =  document.createElement("option");
+
     optA.innerHTML = RachfordRice.chemicals[i];
     optB.innerHTML = RachfordRice.chemicals[i];
 
@@ -32,8 +67,7 @@ for (let i = 0; i < RachfordRice.chemicals.length; i++){
     optArrayB.push(optB);
 }
 
-let preselectA = "n-Pentane";
-let preselectB = "n-Heptane";
+// Preselecting components
 for (let opt of optArrayA){
     if (opt.innerHTML == preselectA){
         opt.selected = true;
@@ -45,38 +79,32 @@ for (let opt of optArrayB){
     }
 }
 
-var Tmin = 0;  Tslider.min = Tmin;
-var Tmax = 200; Tslider.max = Tmax;
-var T = 50; Tslider.value = 50;
+// Setting the slider range
+Tslider.min = Tmin;   Tslider.max = Tmax;
+Pslider.min = Pmin; Pslider.max = Pmax;
 
-var Pmin = 1; Pslider.min = Pmin;
-var Pmax = 1000; Pslider.max = Pmax;
-var P = 101; Pslider.value = 101;
+// Setting the slider values
+Tslider.value = T;
+Pslider.value =  P;
+Zslider.value = 100*zA;
 
-var zA = Zslider.value/100 ;
 
-// let P = 101; // kPa
 let compA = optArrayA.filter(x=>x.selected)[0].innerHTML;
 let compB = optArrayB.filter(x=>x.selected)[0].innerHTML;
 let components = [compA,compB];
 let z = [zA,1-zA];
 
-var R = new RachfordRice(2,T , P, components, z);
+let R = new RachfordRice(2,T , P, components, z);
 let y0 = R.y[0];
 let x0 = R.x[0];
 let vF = Math.min(Math.max(R.v, 0), 1); // Mole Fraction of vapor molecules
 
-let volVap = 0.8; // Volume fraction of Vapor
 let specs = [y0, x0, vF, volVap];
 
 // Just create a box
-let padding = 10;
 var box = new Box(paper, padding);
 
 // Create a lid
-const lidH = 0;
-const particleRadius = 10;
-let numParticles = 20;
 var lid = new Lid(paper, box, lidH, particleRadius, numParticles, specs);
 
 
@@ -85,7 +113,6 @@ var vaporPhase = lid.vaporPhase;
 var liquidPhase = lid.liquidPhase;
 
 // Create Particle
-
 let n0 = Math.round(R.z[0] * numParticles);
 let n1 = numParticles - n0;
 let nVap = Math.round(vF * numParticles);
@@ -96,7 +123,7 @@ let nx0 = Math.round(x0 * nLiq);
 let nx1 = nLiq - nx0;
 
 // Create Exchange Record
-var ExchangeRecord = new Exchange(ny0, ny1, nx0, nx1);
+let ExchangeRecord = new Exchange(ny0, ny1, nx0, nx1);
 
 let particleArray = Array(numParticles);
 
@@ -114,14 +141,14 @@ for (let i = 0; i < numParticles; i++) {
 
 Tslider.addEventListener("input",function(ev){
     T = Tslider.value
-    recalcRachfordRice(ExchangeRecord, T);
+    recalcRachfordRice();
     plot.T = T;
     plot.plot_yx_Tslider(Tmin,Tmax,1);
 });
 
 Pslider.addEventListener("input", function(ev){
     P = Pslider.value;
-    recalcP(ExchangeRecord,P);
+    recalcRachfordRice();
     plot.P = P;
     plot.plot_yx_Tslider(Tmin,Tmax,1)
 });
@@ -129,7 +156,8 @@ Pslider.addEventListener("input", function(ev){
 Zslider.addEventListener("input", function(ev){
     zA = Zslider.value/100;
     z = [zA,1-zA];
-    recalcZ(ExchangeRecord,z)
+    recalcRachfordRice();
+    recalibrateExchangeRecord();
     plot.z = z;
     plot.plot_yx_Tslider(Tmin,Tmax,1);
 
@@ -139,75 +167,42 @@ compAselect.addEventListener("change", function(ev){
     let compA = optArrayA.filter(x=>x.selected)[0].innerHTML;
     let compB = optArrayB.filter(x=>x.selected)[0].innerHTML;
     let components = [compA,compB];
+    recalcRachfordRice();
     plot.components = components;
     plot.plot_yx_Tslider(Tmin,Tmax,1);
-    recalcComponent(ExchangeRecord,components);
 })
 
 compBselect.addEventListener("change", function(ev){
     let compA = optArrayA.filter(x=>x.selected)[0].innerHTML;
     let compB = optArrayB.filter(x=>x.selected)[0].innerHTML;
     let components = [compA,compB];
+    recalcRachfordRice();
     plot.components = components;
     plot.plot_yx_Tslider(Tmin,Tmax,1);
-    recalcComponent(ExchangeRecord,components);
 })
 
-function recalcRachfordRice(exchange, T) {
-    let R = new RachfordRice(2,T , P, components, z);
-    let y0 = R.y[0];
-    let x0 = R.x[0];
-    let vF = Math.min(Math.max(R.v, 0), 1); // Mole Fraction of vapor molecules
-    let n0 = Math.round(R.z[0] * numParticles);
-    let n1 = numParticles - n0;
-    let nVap = Math.round(vF * numParticles);
-    let nLiq = numParticles - nVap;
-    let ny0 = Math.round(y0 * nVap);
-    let ny1 = nVap - ny0;
-    let nx0 = Math.round(x0 * nLiq);
-    let nx1 = nLiq - nx0;
+function recalcRachfordRice() {
+    R = new RachfordRice(2,T , P, components, z)
+    y0 = R.y[0];
+    x0 = R.x[0];
+    vF = Math.min(Math.max(R.v, 0), 1); // Mole Fraction of vapor molecules
+    n0 = Math.round(R.z[0] * numParticles);
+    n1 = numParticles - n0;
+    nVap = Math.round(vF * numParticles);
+    nLiq = numParticles - nVap;
+    ny0 = Math.round(y0 * nVap);
+    ny1 = nVap - ny0;
+    nx0 = Math.round(x0 * nLiq);
+    nx1 = nLiq - nx0;
     let target = [ny0, ny1, nx0, nx1];
-    exchange.setTarget(target);
+    ExchangeRecord.setTarget(target);
 }
 
-function recalcP(exchange, P) {
-    let R = new RachfordRice(2,T , P, components, z);
-    let y0 = R.y[0];
-    let x0 = R.x[0];
-    let vF = Math.min(Math.max(R.v, 0), 1); // Mole Fraction of vapor molecules
-    let n0 = Math.round(R.z[0] * numParticles);
-    let n1 = numParticles - n0;
-    let nVap = Math.round(vF * numParticles);
-    let nLiq = numParticles - nVap;
-    let ny0 = Math.round(y0 * nVap);
-    let ny1 = nVap - ny0;
-    let nx0 = Math.round(x0 * nLiq);
-    let nx1 = nLiq - nx0;
-    let target = [ny0, ny1, nx0, nx1];
-    exchange.setTarget(target);
-}
-
-function recalcZ(exchange, z){
-    let R = new RachfordRice(2,T , P, components, z);
-    let y0 = R.y[0];
-    let x0 = R.x[0];
-    let vF = Math.min(Math.max(R.v, 0), 1); // Mole Fraction of vapor molecules
-    let n0 = Math.round(R.z[0] * numParticles);
-    let n1 = numParticles - n0;
-    let nVap = Math.round(vF * numParticles);
-    let nLiq = numParticles - nVap;
-    let ny0 = Math.round(y0 * nVap);
-    let ny1 = nVap - ny0;
-    let nx0 = Math.round(x0 * nLiq);
-    let nx1 = nLiq - nx0;
-    let target = [ny0, ny1, nx0, nx1];
-    
-    exchange.setTarget(target);
-    
+function recalibrateExchangeRecord(){
     let current = particleArray.filter(x=>x.comType == 0).length; // old n0
-    // Changing the particle colors
     let i = 0;
 
+    // Change the colors for components A and B
     while (current != n0 && i < particleArray.length){
         console.log("current",current,"target",n0);
         if (particleArray[i].comType == 0 && current > n0){
@@ -224,96 +219,33 @@ function recalcZ(exchange, z){
     let ny1_c = particleArray.filter(x=>x.comType==1 && x.phaseType == "V").length;
     let nx0_c = particleArray.filter(x=>x.comType==0 && x.phaseType == "L").length;
     let nx1_c = particleArray.filter(x=>x.comType==1 && x.phaseType == "L").length;
-    exchange.setRecord([ny0_c,ny1_c,nx0_c,nx1_c]);
-
+    ExchangeRecord.setRecord([ny0_c,ny1_c,nx0_c,nx1_c]);
 }
 
-function recalcComponent(exchange,components){
-    let R = new RachfordRice(2,T , P, components, z);
-    let y0 = R.y[0];
-    let x0 = R.x[0];
-    let vF = Math.min(Math.max(R.v, 0), 1); // Mole Fraction of vapor molecules
-    let n0 = Math.round(R.z[0] * numParticles);
-    let n1 = numParticles - n0;
-    let nVap = Math.round(vF * numParticles);
-    let nLiq = numParticles - nVap;
-    let ny0 = Math.round(y0 * nVap);
-    let ny1 = nVap - ny0;
-    let nx0 = Math.round(x0 * nLiq);
-    let nx1 = nLiq - nx0;
-    let target = [ny0, ny1, nx0, nx1];
-    
-    exchange.setTarget(target);
-    
-    let current = particleArray.filter(x=>x.comType == 0).length; // old n0
-    // Changing the particle colors
-    let i = 0;
-
-    while (current != n0 && i < particleArray.length){
-        console.log("current",current,"target",n0);
-        if (particleArray[i].comType == 0 && current > n0){
-            particleArray[i].changeComType(1);
-            current -= 1;
-        } else if(particleArray[i].comType == 1 && current < n0){
-            particleArray[i].changeComType(0);
-            current += 1;
-        }
-        i++;
-    }
-    // label c for "count"
-    let ny0_c = particleArray.filter(x=>x.comType==0 & x.phaseType == "V").length;
-    let ny1_c = particleArray.filter(x=>x.comType==1 && x.phaseType == "V").length;
-    let nx0_c = particleArray.filter(x=>x.comType==0 && x.phaseType == "L").length;
-    let nx1_c = particleArray.filter(x=>x.comType==1 && x.phaseType == "L").length;
-    exchange.setRecord([ny0_c,ny1_c,nx0_c,nx1_c]);
-}
-
-
-// var trace1 = {
-//     x: [1, 2, 3, 4],
-//     y: [10, 15, 13, 17],
-//     mode: 'markers',
-//     type: 'scatter'
-//   };
-  
-//   var trace2 = {
-//     x: [2, 3, 4, 5],
-//     y: [16, 5, 11, 9],
-//     mode: 'lines',
-//     type: 'scatter'
-//   };
-  
-//   var trace3 = {
-//     x: [1, 2, 3, 4],
-//     y: [12, 9, 15, 12],
-//     mode: 'lines+markers',
-//     line: {shape: 'spline'},
-//     type: 'scatter',
-//     name: "name"
-//   };
-  
-//   let data = [trace1, trace2, trace3];
-//   let layout = {
-//     title:'y-x plot',
-//     xaxis: {
-//         title: 'x'
-//       },
-//       yaxis: {
-//         title: 'y'
-//       }
-//   };
-  
-
-//   Plotly.newPlot('Plotly', data,layout);s
-
-  let plot = new Plot('Plotly',T , P, components, z)
-  plot.plot_yx_Tslider(0,200,1)
+// Plot
+let plot = new Plot('Plotly',T , P, components, z)
+plot.plot_yx_Tslider(Tmin_plot,Tmax_plot,step)
   
 // Move particles
 function moveParticles() {
     particleArray.map(p => p.move());
 }
-setInterval(moveParticles, 40);
+let animationLoop = setInterval(moveParticles, 40);
+clearInterval(animationLoop);
+
+let AnimationIsRunning = false;
+function animationStartButtonFunc(){
+    if (!AnimationIsRunning){
+        animationLoop = setInterval(moveParticles,40);
+        AnimationIsRunning = true;
+    } else {
+        clearInterval(animationLoop);
+        AnimationIsRunning = false;
+    }
+}
+
+let animationButton = document.getElementById("animationStartButton")
+animationButton.onclick = animationStartButtonFunc;
 
 
 //----------------------------------------------------------------
